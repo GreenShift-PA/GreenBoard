7710f1db10285b5a0d228fa9ecc147d504ec6c90
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-04-30 17:28:36 +0200

    Rename the generated files

--- src/main/java/com/greenboard/Main.java
+++ src/main/java/com/greenboard/Main.java
@@ -1,4 +1,4 @@
-package com.greenboard.greenboard;
+package com.greenboard;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("auth.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("GreenBoard");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module com.greenboard.greenboard {
     requires javafx.fxml;
 
 
-    opens com.greenboard.greenboard to javafx.fxml;
-    exports com.greenboard.greenboard;
+    opens com.greenboard to javafx.fxml;
+    exports com.greenboard;
 }
